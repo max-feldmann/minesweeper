95b2eb789358b66a48a42bc283bd201eb0d99f06
Max Feldmann <maxfeldmann.consultant@gmail.com>
2020-04-18 14:53:24 +0200

    Added Functionality to Find all Neighboring Tiles of a given Tile.

--- tile.rb
+++ tile.rb
@@ -1,9 +1,15 @@
 class Tile
 
-    attr_reader :bomb, :flagged, :value
+    DIAGIONAL_NEIGHBOURS = [[1,1], [1,-1], [-1,-1], [-1,1]]
+    CONTIGOUS_NEIGHBOURS = [[0,1], [1,0], [-1,0], [0,-1]]
 
-    def initialize(position)
+    attr_reader :bomb, :flagged, :value, :grid_size
+
+    def initialize(position, grid_size)
         @position = position
+
+        @grid_size = grid_size
+        @max_index = grid_size - 1
 
         @bomb = false
         @flagged = false
@@ -34,7 +40,20 @@ class Tile
     end
 
     def neighbor_positions #=> Returns an array of the neigbouring-positions
+        all_possible_neighbours = CONTIGOUS_NEIGHBOURS + DIAGIONAL_NEIGHBOURS
+        neighbours = []
 
+        all_possible_neighbours.each do |neighbour|
+            x,y = (@position[0] + neighbour[0]), (@position[1] + neighbour[1])
+            neighbour_position = [x,y]
+
+            neighbours << neighbour_position if neighbour_on_grid(neighbour_position)
+        end
+
+        p neighbours
     end
 
+    def neighbour_on_grid(neighbour_position)
+        neighbour_position.all? {|coordinate| coordinate.between?(0, @max_index)}
+    end
 end
